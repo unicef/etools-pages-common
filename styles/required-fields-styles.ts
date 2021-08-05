397f138923ef5f08e7bcb8b443498b555a5ef350
Adriana Trif <trif.corina.adriana@gmail.com>
2021-08-05 12:26:20 +0300

    add files

--- styles/required-fields-styles.ts
+++ styles/required-fields-styles.ts
@@ -1,31 +1,11 @@
 import {html} from 'lit-element';
 
 export const RequiredFieldsStyles = html`
-  <style>
-    paper-input[required][label],
-    paper-textarea[required][label],
-    paper-input-container[required],
-    datepicker-lite[required],
-    etools-upload[required],
-    etools-currency-amount-input[required] {
-      --paper-input-container-label: {
-        @apply --required-star-style;
-        color: var(--secondary-text-color, #737373);
-      }
-      --paper-input-container-label-floating: {
-        @apply --required-star-style;
-        color: var(--secondary-text-color, #737373);
-      }
-    }
-
-    etools-dropdown-multi[required]::part(esmm-label),
-    etools-dropdown[required]::part(esmm-label) {
-      @apply --required-star-style;
-    }
-
-    label[required] {
-      @apply --required-star-style;
-      background: url('./images/required.svg') no-repeat 87% 40%/6px;
-    }
-  </style>
+  paper-input[required][label], paper-textarea[required][label], paper-input-container[required],
+  datepicker-lite[required], etools-upload[required], etools-currency-amount-input[required] {
+  --paper-input-container-label: { @apply --required-star-style; color: var(--secondary-text-color, #737373); }
+  --paper-input-container-label-floating: { @apply --required-star-style; color: var(--secondary-text-color, #737373); }
+  } etools-dropdown-multi[required]::part(esmm-label), etools-dropdown[required]::part(esmm-label) { @apply
+  --required-star-style; } label[required] { @apply --required-star-style; background: url('./images/required.svg')
+  no-repeat 87% 40%/6px; }
 `;
